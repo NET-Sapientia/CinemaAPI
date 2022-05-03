feacbc37b10e7ce72b11a277345855dc5b90527e
MNandor <mnandor@users.noreply.github.com>
2022-05-03 06:21:46 -0700

    Start V2 based on template

---
+++ API/API/Controllers/CinemasController.cs
@@ -0,0 +1,26 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CinemasController(AppDbContext context)  //konstruktor
+        {
+            _context = context;
+        }
+
+        //If you want u can get all the Producers -->
+        public async Task<IActionResult> Index()
+        {
+            var allCinemas = await _context.Cinemas.ToListAsync();
+
+            //API-T get, set, hogy ne az adatbazisbol kerje le hanem mashonnan 
+            return View(allCinemas);
+        }
+    }
+}
---
+++ API/API/Migrations/20220321192038_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Algorithms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Icon = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AlgorithmNickname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsPublished = table.Column<bool>(type: "bit", nullable: true),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Algorithms", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Algorithms");
+        }
+    }
+}
